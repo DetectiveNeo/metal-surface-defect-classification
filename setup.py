98c46a9585ac6a6c6b9c188364d4709d57d6bff4
AbhishekNeo <meghwalabhishek1@gmail.com>
2025-12-07 16:03:43 +0530

    Initial project structure created with Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project focuses on classifying six types of metal surface defects using a Convolutional Neural Network (CNN).',
+    author='Abhishek Meghwal',
+    license='',
+)

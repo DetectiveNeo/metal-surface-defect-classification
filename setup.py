5d5a0e42129a9f242eb61b5ebeee5ac3ee682274
AbhishekNeo <meghwalabhishek1@gmail.com>
2025-12-11 15:49:44 +0530

    test push

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
     version='0.1.0',
     description='This project focuses on classifying six types of metal surface defects using a Convolutional Neural Network (CNN).',
     author='Abhishek Meghwal',
-    license='',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  # <-- IMPORTANT FOR src/ LAYOUT
 )
